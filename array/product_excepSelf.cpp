69d040d062cba88e2ba4766a4ab2aa961eb83653
neha1203-coder <nehaskumari1203@gmail.com>
2026-03-06 22:45:14 +0530

    code udpate

--- array/product_excepSelf.cpp
+++ array/product_excepSelf.cpp
@@ -5,34 +5,34 @@ int main(){
     int n = sizeof(arr)/sizeof(arr[0]);
     int result[n];
 
-    // for(int i=0; i<n; i++){
-    //     int product=1;
-    //     for(int j=0; j<n; j++){
-    //         if(i!=j){
-    //             product*=arr[j];
-    //         }
-    //     }
-    //     result[i]=product;
-    // }
-    // for(int i=0; i<n; i++){
-    //     cout<<result[i]<<" ";
-    // }
-    // return 0;
-
-
-
-    result[0]=1;
-    for(int i=1; i<n; i++){
-        result[i] = result[i-1]*arr[i-1];
-    }
-
-    int right=1;
-    for(int i=n-1; i>=0; i--){
-        result[i] = result[i]*right;
-        right = right*arr[i];
+    for(int i=0; i<n; i++){
+        int product=1;
+        for(int j=0; j<n; j++){
+            if(i!=j){
+                product*=arr[j];
+            }
+        }
+        result[i]=product;
     }
     for(int i=0; i<n; i++){
         cout<<result[i]<<" ";
     }
     return 0;
-}
+
+
+
+//     result[0]=1;
+//     for(int i=1; i<n; i++){
+//         result[i] = result[i-1]*arr[i-1];
+//     }
+
+//     int right=1;
+//     for(int i=n-1; i>=0; i--){
+//         result[i] = result[i]*right;
+//         right = right*arr[i];
+//     }
+//     for(int i=0; i<n; i++){
+//         cout<<result[i]<<" ";
+//     }
+//     return 0;
+// }
