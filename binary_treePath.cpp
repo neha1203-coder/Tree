58cfbd92266614567abc7fea8c8102f8b1b19ef3
neha1203-coder <nehaskumari1203@gmail.com>
2026-03-22 17:05:00 +0530

    initial commit

--- binary_treePath.cpp
+++ binary_treePath.cpp
@@ -24,3 +24,35 @@ Node* buildTree(vector<int> preorder){
 
     return root;
 }
+void getPaths(Node* root, string path, vector<string> &ans){
+    if(root == NULL)
+    return;
+
+    if(path.size() == 0){
+       path = to_string(root->data);
+    }
+    else{
+        path += "->" + to_string(root->data);
+    } 
+    if(root->left == NULL && root->right == NULL){
+        ans.push_back(path);
+        return;
+    }
+    getPaths(root->left, path, ans);
+    getPaths(root->right, path, ans);
+}
+int main(){
+    vector<int> preorder = {1, 2, -1, 5, -1, -1, 3, -1, -1};
+    Node* root = buildTree(preorder);
+
+    vector<string> paths;
+    getPaths(root, "", paths);
+    cout << "binary tree paths:" << endl;
+    for(int i = 0; i<paths.size(); i++){
+        string p = paths[i];
+        cout << p << endl;
+    }
+
+    
+    return 0;
+}
